acd5d1d7c7f7d358275590bee05de548b54870f3
Joffrey Janiec <jjaniec@student.42.fr>
2018-08-19 19:36:11 +0200

    hyper: add some plugins to reduce my productivity

--- hyper-terminal/.hyper.js
+++ hyper-terminal/.hyper.js
@@ -131,7 +131,8 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ['hyperpower', 'hyper-search', 'verminal'],
+  plugins: ['hyperpower', 'hyper-search', 'verminal', 'gitrocket', 'space-pull', 'hyper-spotify'],
+  //broken: hypergoogle, hyperminimal
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
