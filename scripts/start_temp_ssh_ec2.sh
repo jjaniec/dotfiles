ef0b6d8548b974446c7a6b14e6ee981ff410ff67
Joffrey Janiec <jjaniec@student.42.fr>
2019-10-26 13:47:49 +0200

    vscode & vpc&subnet args in start_temp_ssh_ec2.sh

--- scripts/start_temp_ssh_ec2.sh
+++ scripts/start_temp_ssh_ec2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #d Create a temp security group for current ip, start an ec2 instance and ssh to it
 
-#u ./start_temp_ssh_ec2.sh
+#u AWS_ACCESS_KEY_ID='' AWS_SECRET_ACCESS_KEY='' ./start_temp_ssh_ec2.sh [vpc-id subnet-id]
 
 set -o nounset
 #set -o xtrace
@@ -10,8 +10,21 @@ set -o errexit
 EC2_AMI="ami-0ce71448843cb18a1" # Amazon Linux 2
 KEYPAIR_NAME="DefaultKPIreland"
 KEYPAIR_PATH='~/Downloads/DefaultKPIreland.pem'
-cur_ip=$(curl -s ifconfig.co)
+CONNECT_MAX_RETRY=60
+cur_ip=$(curl -4 -s ifconfig.co)
 tmp_loc=$(mktemp)
+
+if [ "$#" -ne 2 ];
+then
+  echo "Using default VPC & subnets"
+  vpc_id_fmt="";
+  subnet_id_fmt="";
+else
+  echo "Using vpc: ${1} - subnet: ${2}"
+  vpc_id_fmt="--vpc-id ${1}"
+  subnet_id_fmt="--subnet-id ${2}"
+fi;
+
 
 echo "Current ip: ${cur_ip}"
 
@@ -23,6 +36,8 @@ function connect_instance_id () {
       '.Reservations[0] .Instances[0] .PublicIpAddress'
   )
   echo "Connecting to ${instance_pubip}"
+
+  osascript -e "display notification \"${instance_pubip} up\" with title \"${0}\"" || true
   ssh \
     -i ${KEYPAIR_PATH} \
     -o 'StrictHostKeyChecking=no' \
@@ -30,11 +45,11 @@ function connect_instance_id () {
 }
 
 echo -n "Creating temporary security group ... "
-
 SG_ID=$(\
   aws ec2 create-security-group \
     --description "Allow ssh from ${cur_ip}" \
-    --group-name 'ssh22tcp-current_ip-tmp' \
+    --group-name "ssh22-tcp-${cur_ip}-tmp${RANDOM}" \
+    ${vpc_id_fmt} \
   | jq -r '.GroupId'
 )
 echo "✔️  (${SG_ID})"
@@ -55,6 +70,7 @@ aws ec2 run-instances \
   --key-name ${KEYPAIR_NAME} \
   --security-group-ids ${SG_ID} \
   --associate-public-ip-address  \
+  ${subnet_id_fmt} \
   > ${tmp_loc}
 
 instance_id=$(\
@@ -66,22 +82,48 @@ echo "✔️  (${instance_id})"
 
 echo "Waiting instance ${instance_id} to be ready ... "
 
-while [ 1 ];
+i=0
+while [ ${i} -lt ${CONNECT_MAX_RETRY} ];
 do
     aws ec2 describe-instance-status \
       --instance-ids ${instance_id} \
       > ${tmp_loc}
     instance_state=$(cat ${tmp_loc} | jq -r '.InstanceStatuses[0] .InstanceState .Name')
     instance_status=$(cat ${tmp_loc} | jq -r '.InstanceStatuses[0] .InstanceStatus .Status')
-    echo -n -e "\tState: ${instance_state} - Status: ${instance_status} ... "
-    if [ "${instance_state}" = "running" ] && [ "${instance_status}" == "ok" ];
+    instance_pubip=$(
+      aws ec2 describe-instances \
+        --filters Name=instance-id,Values=${instance_id} \
+      | jq -r \
+        '.Reservations[0] .Instances[0] .PublicIpAddress'
+    )
+
+    echo -n -e "(retry ${i}/${CONNECT_MAX_RETRY})\tState: ${instance_state}\tStatus: ${instance_status}\t\tPublic IP: ${instance_pubip}"
+    #connect ${instance_pubip} exit
+    set -e
+    ssh_exit_code=0
+    ssh \
+      -i ${KEYPAIR_PATH} \
+      -o 'StrictHostKeyChecking=no' \
+      -o 'ConnectTimeout=3' \
+      -o 'ConnectionAttempts=1' \
+      -q \
+      ec2-user@${instance_pubip} exit 0 || ssh_exit_code=$?
+    #echo "Last exit code: ${ssh_exit_code}"
+    if [ ${ssh_exit_code} -eq 0 ];
     then
-      echo '✔️'
-      connect_instance_id ${instance_id}
+      echo -e "\t✔️"
+      osascript -e "display notification \"${instance_pubip} up\" with title \"${0}\"" || true
+      ssh \
+        -i ${KEYPAIR_PATH} \
+        -o 'StrictHostKeyChecking=no' \
+        -q \
+        ec2-user@${instance_pubip}
+      #connect_instance_id ${instance_id}
       break ;
     fi;
-    echo -n -e "                                                                                                      \r"
-    sleep 2
+    echo -n -e "\r"
+    sleep 1
+    i=$((i+1));
 done;
 
 echo -n "Terminating instance ${instance_id} ... "
@@ -91,6 +133,8 @@ aws ec2 terminate-instances \
 aws ec2 wait instance-terminated \
   --instance-ids ${instance_id}
 echo '✔️'
+
+osascript -e "display notification \"Instance ${instance_id} terminated\" with title \"${0}\"" || true
 
 echo -n "Deleting temporary security group ${SG_ID} "
 aws ec2 delete-security-group \
