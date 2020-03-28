71c7a30b8a131f03a550f810d955207c447254df
Joffrey Janiec <jjaniec@student.42.fr>
2020-03-28 11:12:29 +0100

    42 C Project template

---
+++ templates/c-42-linux/includes/ft_project.h
@@ -0,0 +1,6 @@
+#ifndef FT_PROJECT_H
+# define FT_PROJECT_H
+
+# include <ft_printf.h>
+
+#endif
---
+++ templates/c-42-linux/srcs/main.c
@@ -0,0 +1,9 @@
+#include <ft_project.h>
+
+int		main(int ac, char **av, char **environ)
+{
+	(void)ac;
+	(void)av;
+
+	return (42);
+}
