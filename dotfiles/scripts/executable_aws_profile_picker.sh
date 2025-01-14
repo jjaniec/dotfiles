a3a67fdd6bb5b86ee70279bacda6f02f9fac1b61
Joffrey Janiec <16088893+jjaniec@users.noreply.github.com>
2025-01-14 12:18:09 +0100

    Update zshrc

--- dotfiles/scripts/executable_aws_profile_picker.sh
+++ dotfiles/scripts/executable_aws_profile_picker.sh
@@ -3,12 +3,31 @@
 
 #u aws_profile_picker.sh
 
+# set -o verbose
 set -o errexit
 set -o pipefail
 
-export AWS_PROFILE="$(cat ~/.aws/config | grep -E ']$' | cut -d ' ' -f 2 | tr -d '[]' | fzf)"
-ASSUMED_ARN=$(aws sts get-caller-identity --query Arn | tr -d '"')
+TEMP=$(mktemp)
+
+PROFILE="$(cat ~/.aws/config | grep -E ']$' | cut -d ' ' -f 2 | tr -d '[]' | fzf)"
+export AWS_PROFILE="${PROFILE}"
+
+set +o errexit
+aws sts get-caller-identity --query Arn > ${TEMP}
+if [ $? -eq 1 ];
+then
+	echo "${PROFILE}" | grep -q "sso" 2>/dev/null
+	if [ $? -eq 1 ];
+        then
+		aws sso login
+		[ $? -eq 1 ] && (echo "failed"; exit 1)
+	fi;
+fi;
+set -o errexit
+ASSUMED_ARN=$(cat "${TEMP}" | tr -d '"')
 echo "[STS] Now using: ${ASSUMED_ARN}"
 
 set +o errexit
 set +o pipefail
+
+$SHELL
